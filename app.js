f6dcd092770be5bbd6cb95d91c69dd208aaf9aee
Keilin99 <mayopb99@gmail.com>
2022-02-03 14:56:57 -0600

    Agredando archivo app.js con los diferentes tipos de variables a imprimir en consola

---
+++ app.js
@@ -0,0 +1,65 @@
+// Tipo String
+const cadena1 = "Mi nombre es Keilin";
+console.log(cadena1);
+var cadena2 = "Mi primer apellido es Paniagua";
+console.log(cadena2);
+let cadena3 = "Mi segundo apellido es Barrantes";
+console.log(cadena3);
+
+// Tipo Number
+const numero1 = 456;
+console.log(numero1);
+var numero2 = 33.66;
+console.log(numero2);
+let numero3 = 1369;
+console.log(numero3);
+
+// Tipo Boolean
+const valor1 = false;
+console.log(valor1);
+var valor2 = true;
+console.log(valor2);
+let valor3 = false;
+console.log(valor3);
+
+// Tipo Array[]
+const arreglo1 = ["Mango", "Pera", "Guayaba"];
+console.log(arreglo1[1]);
+var arreglo2 = ["Piña", "Papaya", "Banano"];
+console.log(arreglo2[2]);
+let arreglo3 = ["Fresa", "Manzana", "Kiwi"];
+console.log(arreglo3[0]);
+
+// Tipo Object
+const objeto1 = {
+  nombre: "Keilin",
+  edad: 22,
+  colorFavorito: "verde",
+};
+console.log(objeto1);
+
+var objeto2 = {
+  nombre: "Nathalie",
+  edad: 27,
+  colorFavorito: "azul",
+};
+console.log(objeto2.nombre);
+
+let objeto3 = {
+  nombre: "Sofia",
+  edad: 12,
+  colorFavorito: "morado",
+};
+console.log(objeto3.colorFavorito);
+
+// Tipo Undefined
+
+// IMPORTANTE: una variable de tipo const se debe de inicializar
+// const esUndefined1;
+// console.log(esUndefined1);
+
+var esUndefined2;
+console.log(esUndefined2);
+
+let esUndefined3;
+console.log(esUndefined3);
